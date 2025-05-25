ba2324afeb5445cd7b814cfb741b2dcec3c0b6ba
Kalle Rantalainen <kllrantalainen366@gmail.com>
2025-05-25 12:45:57 +0300

    [ADD] lisää teemoja ja tyylejä

--- app/static/js/themeSwitch.js
+++ app/static/js/themeSwitch.js
@@ -7,4 +7,10 @@ function switchTheme(themeName) {
 window.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || 'theme-default';
     switchTheme(savedTheme);
+
+    // Set the select element's value to match the saved theme
+    const themeSelector = document.querySelector('.dropdown');
+    if (themeSelector) {
+        themeSelector.value = savedTheme;
+    }
 });
